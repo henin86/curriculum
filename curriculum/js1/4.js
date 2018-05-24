05464493e0b3ef2d5504e81873293dffd4a7e97b
Ping Yang <henin86@garagescript.org>
2018-05-23 19:44:57 -0700

    js1 one through 5

--- curriculum/js1/4.js
+++ curriculum/js1/4.js
@@ -6,8 +6,10 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a,b,c='') => 
+{
+  if (a===0) return c;
+  return solution (a-1,b, c+b);
 };
 
 module.exports = {
